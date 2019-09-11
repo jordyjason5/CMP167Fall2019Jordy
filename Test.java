41ed2cd2093eb1e7f01973221a16e76a1bf9f922
jordyjason5 <41080283+jordyjason5@users.noreply.github.com>
2019-09-11 12:29:30 -0400

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,19 @@
+public class Test {
+	public static void main(String[] args) {
+		System.out.println(78+98); 
+		System.out.println('a'+'b');
+		System.out.println('a'-'b');
+		System.out.println('0'+'0');
+		System.out.println('a'+1);
+		System.out.println('a');
+		
+		int num = 10;
+		double dollars = 15.60;
+		String name1 = "Jordy";
+		String name2 = "Kathryn";
+		System.out.printf("I have %d cars", num);
+		System.out.printf(" Maria has %.2f in her pocket", dollars);
+		
+		System.out.printf("%n %s is %d years older than %s", name1, 3, name2);
+	}
+}
